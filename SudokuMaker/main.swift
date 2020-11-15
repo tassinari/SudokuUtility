80a1123106a455a8586b0ecb4b54f7cf79aa0b6a
Mark Tassinari <tassinari@gmail.com>
2020-11-15 09:27:35 -0500

    Added Puzzle maker makes puzzles and adds to core data db

--- SudokuMaker/main.swift
+++ SudokuMaker/main.swift
@@ -9,7 +9,7 @@
 import Foundation
 import SudukoUtility
 import ArgumentParser
-import SQLite
+import CoreData
 
 
 let defaultPuzzleNumber = 100
@@ -17,65 +17,84 @@ let defaultPuzzleNumber = 100
 struct Repeat: ParsableCommand {
     @Flag(name: [.customLong("verbose"), .customShort("v")],help: "Verbose mode")
     var includeCounter = false
-
+    
     @Option(name: [.customLong("max-puzzles"), .customShort("m")], help: "Number of puzzles to add (default is \(defaultPuzzleNumber))")
     var maxPuzzles: Int?
-
+    
     @Argument(help: "Path to exsisting SQLite Database, a new one will be created here if not found")
     var sqlPath: String
-
+    
     mutating func run() throws {
-        
-        do{
-            let db = try createDB(path: sqlPath)
-            try addPuzzles(db: db, count: 10)
-        }catch let e{
-            print("DB creation failure -- \(e.localizedDescription)")
+        do {
+            try addPuzzles(count: 10)
+        } catch  {
+            
         }
+        
         
     }
 }
 
 Repeat.main()
 
-
-//let tableName = "games"
-//let idCol = "id"
-//let hashCol = "gameHash"
-//let ratingCol = "rating"
-
-func createDB(path : String) throws -> Connection{
-    let db = try Connection(path)
-    let games = Table("games")
-    let id = Expression<Int64>("id")
-    let hash = Expression<String?>("hash")
-    let rating = Expression<Int>("rating")
-
-    try db.run(games.create { t in
-        t.column(id, primaryKey: true)
-        t.column(hash)
-        t.column(rating)
-    })
-    return db
-}
-
-func addPuzzles( db : Connection, count : Int) throws{
-    do{
-        for _ in 0..<count{
-            let p = try SudokuPuzzle.creatPuzzle()
-            let r = try p.rate()
-            let insert = Table("games").insert(Expression<String?>("hash") <- p.base64Hash, Expression<Int>("rating") <- Int(r.rawValue))
-            let rowid = try db.run(insert)
-            print("row inserted \(rowid)")
+func addPuzzles( count : Int) throws{
+    
+    let _ = persistentContainer { per in
+        let moc : NSManagedObjectContext = per.viewContext
+        
+        do{
+            for _ in 0..<count{
+                let p = try SudokuPuzzle.creatPuzzle()
+                let r = try p.rate()
+                let puzzle = Puzzle(context: moc)
+                puzzle.puzzleHash = p.base64Hash
+                puzzle.rating = r.rawValue
+                puzzle.used = false
+                moc.insert(puzzle)
+                
+            }
+            try moc.save()
+            let url = URL(fileURLWithPath: "/Users/tassinari/Developer/SuperDoku/test.sqlite")
+            let d = per.persistentStoreDescriptions
+            
+            let store = per.persistentStoreCoordinator.persistentStores.first!
+            print(d)
+            try per.persistentStoreCoordinator.migratePersistentStore(store, to: url , options: nil, withType: NSSQLiteStoreType)
+        }catch{
+            
         }
         
-        // INSERT INTO "users" ("name", "email") VALUES ('Alice', 'alice@mac.com')
-//        for user in try db.prepare(games) {
-//            print("id: \(user[id]), name: \(user[name]), email: \(user[email])")
-//            // id: 1, name: Optional("Alice"), email: alice@mac.com
-//        }
-    }catch let e{
-        
     }
-   
+    
+    
+}
+
+
+// MARK: - Core Data stack
+func persistentContainer( completion : @escaping ( NSPersistentContainer)->Void) -> NSPersistentContainer {
+    /*
+     The persistent container for the application. This implementation
+     creates and returns a container, having loaded the store for the
+     application to it. This property is optional since there are legitimate
+     error conditions that could cause the creation of the store to fail.
+     */
+    let container = NSPersistentContainer(name: "Puzzle")
+    container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+        if let error = error as NSError? {
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            /*
+             Typical reasons for an error here include:
+             * The parent directory does not exist, cannot be created, or disallows writing.
+             * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+             * The device is out of space.
+             * The store could not be migrated to the current model version.
+             Check the error message to determine what the actual problem was.
+             */
+            fatalError("Unresolved error \(error), \(error.userInfo)")
+        }
+        completion(container)
+        
+    })
+    return container
 }
